e9b4084877d7185856a0306994ec368391e0c9c5
Qingtian Wang <wqt.net@gmail.com>
2022-10-17 21:48:27 -0500

    better readability

--- src/main/java/elf4j/log4j/Log4jLoggerFactory.java
+++ src/main/java/elf4j/log4j/Log4jLoggerFactory.java
@@ -3,6 +3,8 @@ package elf4j.log4j;
 import elf4j.Logger;
 import elf4j.spi.LoggerFactory;
 
+import javax.annotation.Nullable;
+
 public class Log4jLoggerFactory implements LoggerFactory {
     @Override
     public Logger logger() {
@@ -10,12 +12,12 @@ public class Log4jLoggerFactory implements LoggerFactory {
     }
 
     @Override
-    public Logger logger(String name) {
+    public Logger logger(@Nullable String name) {
         return Log4jLogger.instance(name);
     }
 
     @Override
-    public Logger logger(Class<?> clazz) {
+    public Logger logger(@Nullable Class<?> clazz) {
         return Log4jLogger.instance(clazz);
     }
 }
